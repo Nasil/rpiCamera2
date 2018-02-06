f809d47d734283fec4e00ba6ee5f4afa022def25
Nasil <hns4798@naver.com>
2018-02-06 22:40:30 +0900

    add file

---
+++ aruco/otsu2.js
@@ -0,0 +1,156 @@
+'use strict';
+
+let maxSum = 0.;
+let thresholds = [];
+
+function createImg(frame, width, height) {
+	return {
+		width : width,
+		height : height,
+		data : frame
+	}
+}
+
+function histogram(image) {
+    let histogram = new Array(256);
+    histogram.fill(0);
+
+    let maxCnt = image.width * image.height;
+    for (let i = 0; i < maxCnt; i++) {
+        let luma = image.data[i];
+        histogram[luma]++;
+    }
+
+    for (let i = 0; i < histogram.length; i++) {
+        histogram[i]++;
+    }
+
+    return histogram;
+}
+
+function for_loop(H, u, vmax, level, levels, index)
+{
+    var classes = index.length - 1;
+
+    for (var i = u; i < vmax; i++) {
+        index[level] = i;
+
+        if (level + 1 >= classes) {
+            // Reached the end of the for loop.
+
+            // Calculate the quadratic sum of al intervals.
+            var sum = 0.;
+
+            for (var c = 0; c < classes; c++) {
+                var u = index[c];
+                var v = index[c + 1];
+                var s = H[v + u * levels];
+                sum += s;
+            }
+
+            if (maxSum < sum) {
+                // Return calculated threshold.
+                thresholds = index.slice(1, index.length - 1);
+                maxSum = sum;
+            }
+        } else
+            // Start a new for loop level, one position after current one.
+            for_loop(H,
+                    i + 1,
+                    vmax + 1,
+                    level + 1,
+                    levels,
+                    index);
+    }
+}
+
+function buildTables(histogram)
+{
+    // Create cumulative sum tables.
+    var P = new Array(histogram.length + 1);
+    var S = new Array(histogram.length + 1);
+    P[0] = 0;
+    S[0] = 0;
+
+    var sumP = 0;
+    var sumS = 0;
+
+    for (var i = 0; i < histogram.length; i++) {
+        sumP += histogram[i];
+        sumS += i * histogram[i];
+        P[i + 1] = sumP;
+        S[i + 1] = sumS;
+    }
+
+    // Calculate the between-class variance for the interval u-v
+    var H = new Array(histogram.length * histogram.length);
+    H.fill(0.);
+
+    for (var u = 0; u < histogram.length; u++)
+        for (var v = u + 1; v < histogram.length; v++)
+            H[v + u * histogram.length] = Math.pow(S[v] - S[u], 2) / (P[v] - P[u]);
+
+    return H;
+}
+
+function otsuExec(histogram, classes)
+{
+    maxSum = 0.;
+    thresholds = new Array(classes - 1);
+    thresholds.fill(0);
+    let H = buildTables(histogram);
+    let index = new Array(classes + 1);
+    index[0] = 0;
+    index[index.length - 1] = histogram.length - 1;
+
+    for_loop(H,
+            1,
+            histogram.length - classes + 1,
+            1,
+            histogram.length,
+            index);
+
+    return thresholds;
+}
+
+function otsu(frame, width, height)
+{
+	let img = createImg(frame, width, height);
+    let hist = histogram(img);
+
+    let classes = 2;
+    let thresholds = otsuExec(hist, classes);
+	
+    console.log(thresholds);
+
+    var dstData = img.data;
+    var colors = new Array(classes);
+    for (var i = 0; i < classes; i++)
+        colors[i] = Math.round(255 * i / (classes - 1));
+
+    var colorTable = new Array(256);
+    var j = 0;
+
+    for (var i = 0; i < colorTable.length; i++) {
+        if (j < thresholds.length && i >= thresholds[j])
+            j++;
+
+        colorTable[i] = colors[j];
+    }
+
+    for (var i = 0; i < dstData.length; i += 2) {
+        var luma = (  11 * dstData[i]
+                    + 16 * dstData[i + 1]
+                    +  5 * dstData[i + 2]) >> 5;
+        luma = colorTable[luma];
+
+        dstData[i]     = luma;
+        dstData[i + 1] = 255;
+    }
+
+	console.log(dstData);
+	return dstData;
+}
+
+
+exports.otsu2 = otsu;
---
+++ canny/README.md
@@ -0,0 +1,54 @@
+Canny Edge Detection
+====================
+Works the following way:
+
+1. Convert the canvas image to grayscale
+2. Smooth the image to reduce noise as much as possible.
+In this implementation, Gaussian filter can be applied (max kernel size is 21).
+3. Determine the gradient intensity (amount of change) and direction for each pixel.
+This is done by convolving image with the chosen filter.
+Currently, there are 3 operators you may choose from:
+  * Sobel
+  * Prewitts
+  * Cross
+4. Thin the resulting edges with non-maximum suppression.
+5. Remove weak/false edges.
+A process called hysteresis is used where there are two thresholds--high and low--to be compared to each pixel.
+
+## Usage
+You'll need [Node.js](http://nodejs.org/) installed.
+Go to root directory of the project and start by entering the following on your terminal:
+```
+node js/server.js
+```
+Access program on port 8000.
+
+## TODO
++ [ ] Cleanup image uploading.
++ [x] Fix non-maximum suppression thresholding (especially bad with Cross filter)
++ [x] Add other filters
+
+
+## Contributors
+* [@Tagussan](https://github.com/Tagussan): Otsu's method for thresholding.
+
+
+## License
+Distributed under the terms of the [GNU General Public License version 3](http://www.gnu.org/copyleft/gpl.html).
+
+```
+Copyright (C) 2013 cmisenas
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+```
